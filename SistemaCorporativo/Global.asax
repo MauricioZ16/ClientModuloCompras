5839db949e3e8d42d63dc890dabf0a288d5a2eec
MauricioZ16 <33468645+MauricioZ16@users.noreply.github.com>
2019-10-12 16:26:46 -0300

    Add project files.

---
+++ SistemaCorporativo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaCorporativo.MvcApplication" Language="C#" %>
---
+++ SistemaCorporativo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaCorporativo.Startup))]
+namespace SistemaCorporativo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
